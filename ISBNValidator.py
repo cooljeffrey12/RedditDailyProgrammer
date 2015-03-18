3045943bc6a2cb3970d6fa5782d890cab73d0e0b
JinHoon <cooljeffrey12@gmail.com>
2015-03-18 19:20:23 +0900

    validator implemented, generator needs implementation

--- ISBNValidator.py
+++ ISBNValidator.py
@@ -1,17 +1,26 @@
-selector = input("Type V for ISBN Validator or G for ISBN Generator\n")
-selectorCorrect = False
-
-
 while selectorCorrect == False:
+    selector = input("Type V for ISBN Validator or G for ISBN Generator. Type Q to quit\n")
     if (selector == "v" or selector == "V"):
-        selectorCorrect = True
         inputCorrect = False
+        while inputCorrect == False:
             inputISBN = input("Type in a 10-digit ISBN code\n")
-            if len(inputISBN) == 10:
-                try:
-                    int(selector.replace[:9])
-                except TypeError:
-                    print("First 9-digit needs to be an integer")
-                else:
-                    inputCorrect = True
-        print(inputISBN)            # elif (selector == "g" or selector == "G"):
+            if len(inputISBN) == 10 and inputISBN[9] in ["0","1","2","3","4","5","6","7","8","9","x","X"]:
+                inputCorrect = True
+        firstNine = inputISBN[:9]
+        lastDigit = inputISBN[9]
+        multiplier = 10
+        sum = 0
+        for digit in firstNine:
+            sum = sum + (int(digit) * multiplier)
+            multiplier = multiplier - 1
+        if lastDigit == "x" or lastDigit == "X":
+            sum = sum + 10
+        elif lastDigit in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            sum = sum + int(lastDigit)
+        if sum%11 == 0:
+            print(inputISBN, "is valid")
+        else:
+            print(inputISBN, "is NOT valid")
+    # elif selector == "g" or selector == "G":
+    elif selector == "q" or selector == "Q":
+        selectorCorrect = True
