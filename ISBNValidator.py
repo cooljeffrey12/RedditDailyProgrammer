8e49bad6463462ca5b7035af01b02a46c66bd1da
JinHoon <cooljeffrey12@gmail.com>
2015-03-18 19:43:58 +0900

    code is functional. but requires massive cleaning

--- ISBNValidator.py
+++ ISBNValidator.py
@@ -1,3 +1,14 @@
+from random import randint
+
+def sumFirstNine(firstNine):
+    multiplier = 10
+    sum = 0
+    for digit in firstNine:
+        sum = sum + (int(digit) * multiplier)
+        multiplier = multiplier - 1
+    return sum
+
+selectorCorrect = False
 while selectorCorrect == False:
     selector = input("Type V for ISBN Validator or G for ISBN Generator. Type Q to quit\n")
     if (selector == "v" or selector == "V"):
@@ -8,19 +19,23 @@ while selectorCorrect == False:
                 inputCorrect = True
         firstNine = inputISBN[:9]
         lastDigit = inputISBN[9]
-        multiplier = 10
-        sum = 0
-        for digit in firstNine:
-            sum = sum + (int(digit) * multiplier)
-            multiplier = multiplier - 1
+        sum = sumFirstNine(firstNine)
         if lastDigit == "x" or lastDigit == "X":
             sum = sum + 10
-        elif lastDigit in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        elif lastDigit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             sum = sum + int(lastDigit)
         if sum%11 == 0:
             print(inputISBN, "is valid")
         else:
             print(inputISBN, "is NOT valid")
-    # elif selector == "g" or selector == "G":
+    elif selector == "g" or selector == "G":
+        firstNine = randint(100000000,999999999)
+        sum = sumFirstNine(str(firstNine))
+        lastDigit = 11 - (sum%11)
+        if lastDigit == 10:
+            lastDigit = "X"
+        generatedISBN = str(firstNine) + str(lastDigit)
+        print(generatedISBN)
     elif selector == "q" or selector == "Q":
         selectorCorrect = True
+
